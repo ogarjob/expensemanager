edc37ed6b4eeaef0bffed2891bb23782f117d606
ogarjob <ogarjobbassey@gmail.com>
2023-04-19 13:24:31 +0100

    Update navigation layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -66,8 +66,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('expenses.index')" :active="request()->routeIs('expenses.index')">
+                Employees Expenses
             </x-responsive-nav-link>
         </div>
 
